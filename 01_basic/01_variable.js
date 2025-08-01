da5bc3b6b1e73148a205b52454e30b56f617abee
Sarvagya Jain <90783021+SARVAGYAJAIN16@users.noreply.github.com>
2025-08-01 06:05:07 +0000

    datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId =  12345;
+let accountEmail = "sarvagya@github.com";
+var accountPassword = "32145";
+accountCity = "jaipur";
+let accountState;
+
+
+// accountId = 2;
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, accountCity, accountState]);
+
+
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // for newer version ...
+// alert(3 + 3);  // we are using node js not browser
+console.log( 3 + 3)
+console.log("sarvagya")
+
+// datatypes
+let name = "sarvagya"
+let age = 18
+let isLoggedIn = false
+
+console.log(typeof "name")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
