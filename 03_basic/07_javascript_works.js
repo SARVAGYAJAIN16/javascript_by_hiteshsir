2d46665a3994e37b847e7be48f79bdb2fe0dc69d
Sarvagya Jain <90783021+SARVAGYAJAIN16@users.noreply.github.com>
2025-08-13 04:53:51 +0000

    js code working

---
+++ 03_basic/07_javascript_works.js
@@ -0,0 +1,10 @@
+// Understand about how javascript works and about call stack.
+// concept covered
+/*
+ -> javascript exeution context
+ -> global exeution context
+ -> memory phase
+ -> exeution phase
+ -> New exeutional phase
+ -> Call stack
+*/
