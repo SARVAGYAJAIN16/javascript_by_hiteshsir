dfaab8cf2d779e21d7a5714849cead6181c2911b
Sarvagya Jain <90783021+SARVAGYAJAIN16@users.noreply.github.com>
2025-08-14 08:55:46 +0000

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,34 @@
+// Switch
+//syntax
+/*
+  switch(key){
+    case value;
+      break;
+    default;
+     break;  
+  }
+*/ 
+const val =3
+switch(val){
+    case 1:
+    console.log("jan");
+    break;
+    case 2:
+    console.log("feb");
+    break;
+    case 3:
+    console.log("march");
+    case 4:
+    console.log("April");
+
+    default:
+         console.log("default case match");
+         break; 
+}
+// output:- 
+//  march
+//  April
+//  default case match
+
+
+// string also we can take instead of number
