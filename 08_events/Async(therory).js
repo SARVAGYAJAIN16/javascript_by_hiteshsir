0b9f8d62de20bbd6abab80fe2bd8cb7cf7db400b
sarvagya <sarvagyajain516@gmail.com>
2025-08-24 10:22:28 +0530

    async vs sync

---
+++ 08_events/Async(therory).js
@@ -0,0 +1,10 @@
+// Async theory (notes in copy)
+/* 
+-> Understand about synchronous and asynchronous
+-> Js engine
+-> blocking code vs non blocking code
+-> understand about web api , register call back ,task queue
+-> understand  about fetch() and promise (through diagram)
+-> deep understanding of Asynchronous and how it works.
+
+*/
